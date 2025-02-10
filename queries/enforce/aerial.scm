ab4d34b7f7c1c2c9c67b9ece20eb29f2d17079ed
Saitama- <wishiweresaitama@gmail.com>
2025-02-10 18:46:52 +0000

    Add enforce support #4

--- queries/enforce/aerial.scm
+++ queries/enforce/aerial.scm
@@ -2,3 +2,10 @@
   typename: (identifier) @name
   (#set! "kind" "Class")) @symbol
 
+(decl_method
+  name: (identifier) @name
+  (#set! "kind" "Function")) @symbol
+
+(decl_enum
+  typename: (identifier) @name
+  (#set! "kind" "Enum")) @symbol
